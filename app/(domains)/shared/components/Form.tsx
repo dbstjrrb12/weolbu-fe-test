b4eb9c8563b367f0f9411a3d6d25894a93b7fa2f
윤석규 <sg.yun@joongonara.co.kr>
2025-12-20 16:20:00 +0900

    feat: Form 컴포넌트 - Select 컴포넌트 추가

--- app/(domains)/shared/components/Form.tsx
+++ app/(domains)/shared/components/Form.tsx
@@ -1,22 +1,48 @@
-import { type ComponentProps, type PropsWithChildren } from 'react';
+import { type ComponentProps } from 'react';
 
 import { cn } from '../utils/common';
 import Input from './ui/Input';
 import Button from './ui/Button';
+import UISelect from './ui/Select';
 
-interface FormProps extends PropsWithChildren {
-  className?: string;
-}
+interface FormProps extends ComponentProps<'form'> {}
 
-export default function Form({ children, className }: FormProps) {
+export default function Form({ children, className, ...props }: FormProps) {
   return (
-    <form className={cn('flex flex-col gap-4', className)}>{children}</form>
+    <form className={cn('flex flex-col gap-4', className)} {...props}>
+      {children}
+    </form>
   );
 }
 
-function Submit({ ...props }: Omit<ComponentProps<'button'>, 'type'>) {
-  return <Button type="submit" {...props} />;
+function Submit({
+  children,
+  ...props
+}: Omit<ComponentProps<typeof Button>, 'type'>) {
+  return (
+    <Button type="submit" {...props}>
+      {children}
+    </Button>
+  );
+}
+
+function Select({
+  options,
+  ...props
+}: ComponentProps<typeof UISelect> & {
+  options?: Array<{ value: string; label: string }>;
+}) {
+  return (
+    <UISelect {...props}>
+      {options?.map((option) => (
+        <UISelect.Option key={option.value} value={option.value}>
+          {option.label}
+        </UISelect.Option>
+      ))}
+    </UISelect>
+  );
 }
 
 Form.Input = Input;
+Form.Select = Select;
 Form.Submit = Submit;
